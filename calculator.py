5ecfbb440ae148b208346055aeadaa8738b9af2f
MADEDECHADWICK <chadwickmadede@gmail.com>
2025-03-11 13:31:06 +0300

    Pythin week 1 assignment

---
+++ calculator.py
@@ -0,0 +1,20 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+
+def Sum():
+  answer = a + b
+  print(answer)
+  
+def Subtitute():
+  answer = a - b
+  print(answer)
+  
+def Multiply():
+  answer = a * b
+  print(answer)
+
+def Division():
+  answer = a / b
+  print(answer)
+  
+Sum()
